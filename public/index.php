b7d5b3b8502aa046ecf8378c6ce172a1f9d57efa
Paco Gomez <fjgomez@florida-uni.es>
2019-07-15 13:45:15 +0200

    Formulario

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ $nombreCodificado=$c->codifica("Rachid");
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="./css/estilos.css">
     <title>Pagina home del proyecto</title>
   </head>
   <body>
@@ -19,7 +19,17 @@ $nombreCodificado=$c->codifica("Rachid");
   include __DIR__.'/../vistas/menu.php';
   ?>
   <div class="content">
-  <h1><?=$nombreCodificado?></h1>
+  <h1 class="codificado"><?=$nombreCodificado?></h1>
+  </div>
+  <div class="content formulario">
+    <form action="">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Introduce solucion</label>
+        <input type="text" class="form-control" id="exampleInputSol" aria-describedby="solHelp" placeholder="Solucion">
+        <small id="solHelp" class="form-text text-muted">REsuleve el nombre</small>
+      </div>
+      <button type="submit" class="btn btn-primary">RESOLVER</button>
+    </form>
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
