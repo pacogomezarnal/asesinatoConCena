9e9e4e9f41f184a5e4d56da38ca27ada197ef390
Paco Gomez <fjgomez@florida-uni.es>
2019-07-15 12:12:55 +0200

    Inicio de proyecto de aula

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Geeks\codec\Cifrado as Cifrado;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pagina home del proyecto</title>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ src/codec/Cifrado.php
@@ -0,0 +1,6 @@
+<?php
+namespace Geeks\codec;
+
+class Cifrado{
+    
+}
