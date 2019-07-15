f643a2ecaca042191bac8ed64f7b063fac9e2bb2
Paco Gomez <fjgomez@florida-uni.es>
2019-07-15 12:51:23 +0200

    Cifrado

--- src/codec/Cifrado.php
+++ src/codec/Cifrado.php
@@ -2,5 +2,20 @@
 namespace Geeks\codec;
 
 class Cifrado{
-    
+    private $alfabetoBase=null;
+    private $alfabetoCesar=null;
+
+    private function crearAlfabeto($desplaza){
+        for ($i=65; $i < 91 ; $i++) { 
+            $this->alfabetoBase[]=chr($i);
+        }
+    }
+
+    function codifica(){
+        $this->crearAlfabeto(3);
+    }
+
+    function getAlfabetoOriginal(){
+        return $this->alfabetoBase;
+    }
 }
