6a161b28e77a9b549dfd244bd886690fba3798c0
Paco Gomez <fjgomez@florida-uni.es>
2019-07-23 10:09:03 +0200

    DB

--- src/models/Db.php
+++ src/models/Db.php
@@ -1,15 +1,28 @@
 <?php
-namespace Daw\models;
-/**
- * Plantilla vacia de Db
- */
+namespace Geeks\models;
+
 class Db
 {
+  private $host="localhost";
+  private $db="cena";
+  private $user="root";
+  private $pass;
+  public $conexion=null;
+  public $resultado;
 
   function __construct()
   {
   }
 
+  public function conectar(){
+      $error=null;
+      try {
+          $this->conexion = new \PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
+      } catch (PDOException $e) {
+          $error=$e->getMessage();
+      }
+      return $error;
+    }
 }
 
 ?>
